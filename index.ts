03adb558e557e9cdc36c1582701aebe8e2e64925
qphilippot <quentin.philippot@gmail.com>
2022-01-08 14:26:31 +0100

    1.1.3 - expose subscriber.interface

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ export { default as Subscriber } from './src/model/subscriber.model';
 export { default as PublisherSubscriber } from './src/model/publisher-subscriber.model';
 export { default as SubscriptionInterface } from './src/interfaces/subscription.interface';
 export { default as PublisherInterface } from './src/interfaces/publisher.interface';
+export { default as SubscriberInterface } from './src/interfaces/subscriber.interface';
 export { default as PublisherSubscriberInterface } from './src/interfaces/publisher-subscriber.interface';
 export * as SubscriberHelper from './src/helper/subscriber.helper';
 export { default as InvalidArgumentException } from './src/exception/invalid-argument.exception';
